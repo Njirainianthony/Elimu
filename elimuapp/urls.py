7a07bf13b2fea1124612bc839b32ef9e6addb6aa
Njirainianthony <njirainianthony@gmail.com>
2024-07-03 08:55:30 +0300

    initial commit

--- elimuapp/urls.py
+++ elimuapp/urls.py
@@ -5,7 +5,12 @@ from elimuapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home),
-    path('gallery/', views.gallery),
+    path('home/', views.home, name='home'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('info/', views.info, name="info"),
+    path('tableinfo/', views.tableinfo, name='tableinfo'),
+    path('client/', views.clientform, name='form'),
+    path('login/', views.login, name='login'),
+    path('', views.register, name='register'),
 
 ]
--- elimuapp/views.py
+++ elimuapp/views.py
@@ -7,4 +7,19 @@ def home(request):
 def gallery(request):
     return render(request,'gallery.html')
 
+def info(request):
+    return render(request,'info.html')
+
+def tableinfo(request):
+    return render(request,'tableinfo.html')
+
+def clientform(request):
+    return render(request,'form.html')
+
+def login(request):
+    return render(request,'login.html')
+
+def register(request):
+    return render(request,'regform.html')
+
 
